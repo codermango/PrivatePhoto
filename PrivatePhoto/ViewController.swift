f33111d35d3dc72ac5403203dfb02088b9db212d
codermango <codermango@gmail.com>
2015-08-02 23:44:14 +0200

    Start Private Photo project

---
+++ PrivatePhoto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrivatePhoto
+//
+//  Created by Mark on 15/8/2.
+//  Copyright (c) 2015年 codermango. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
